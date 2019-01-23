3599e3b59cf9ec398c1c99a8a210d82cc46bd99a
D072828 <duffyduck451@gmail.com>
2019-01-23 12:22:01 +0100

    optimized performence, solved memoryError

--- venv/my_plot.py
+++ venv/my_plot.py
@@ -20,92 +20,103 @@ from sklearn.utils.fixes import signature
 import itertools
 import math
 
-N = [1000,2000,3000, 4000, 5000, 6000, 10000]
+N = [10000,20000,30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+# N = [5000, 10000]
 plot_f1 = []
 mse_array = []
 std_dev_array = []
 std_error_array = []
 
 
-def calculate_avg_f1_per_dataset(slice):
+def calculate_f1_array_per_dataset(slices):
     # classifier = GaussianNB()
     # classifier = SVC(kernel='rbf') # Gives warning
     classifier = LinearSVC(random_state=0, tol=1e-5, C=1)
     # classifier = KNeighborsClassifier(n_neighbors=3)
     corpus = []
     y = []
-    with open('C:/Users/D072828/PycharmProjects/Master-Thesis/venv/preprocessed_1k.csv', newline='') as csvfile:
+    plot_f1 = []
+    with open('C:/Users/D072828/PycharmProjects/Master-Thesis/venv/preprocessed_polarity_100k_v1.csv', newline='') as csvfile:
         yelp = csv.reader(csvfile, delimiter=',')
-        for row in itertools.islice(yelp, slice):
-            clean_row = row[1].strip().replace('"', '').replace(';', '')
-            target_class = row[0]
-            corpus.append(clean_row)
-            y.append(int(target_class))
-    tfidf_vectorizer = TfidfVectorizer(norm='l2')
-    matrix = tfidf_vectorizer.fit_transform(corpus)
-    sparse = tfidf_vectorizer.fit_transform(corpus).A
-    print(pd.DataFrame(matrix.todense(), columns=tfidf_vectorizer.get_feature_names()))
-    #Cross validation
-    print("CROSS VALIDATION:")
-    splits = 10
-    kf_total = StratifiedKFold(n_splits=splits, shuffle=True)
-    kf = KFold(n_splits=10)
-    y1 = np.array(y)
-    avg_f1 = 0
-    avg_precision = 0
-    avg_recall = 0
-    avg_mse = 0
-    avg_std_dev = 0
-    avg_std_error = 0
-    start = time.time()
-    for train_index, test_index in kf_total.split(sparse, y1):
-        X_train, X_test = sparse[train_index], sparse[test_index]
-        y_train, y_test = y1[train_index], y1[test_index]
-        classifier.fit(X_train, y_train)
-        y_pred = classifier.predict(X_test)
-        f1 = f1_score(y_test, y_pred, average='macro')  # y_test = true y, y_pred = predicted y with the classifier
-        mse = mean_squared_error(y_test, y_pred);
-        std_dev = np.std([y_test, y_pred])
-        std_error = std_dev/math.sqrt(slice)
-        precision = precision_score(y_test, y_pred, average='macro')
-        recall = recall_score(y_test, y_pred, average='macro')
-        print('F1 score: ', f1)
-        print('Precision score: ', precision)
-        print('Recall score: ', recall)
-        print('Mean squared Error: ', mse)
-        print('Standard Deviation: ', std_dev)
-        print('Standard Error: ', std_error)
-        avg_f1 += f1
-        avg_precision += precision
-        avg_recall += recall
-        avg_mse += mse
-        avg_std_dev += std_dev
-        avg_std_error += std_error
-    end = time.time()
-    avg_recall = avg_recall / splits
-    avg_precision = avg_precision / splits
-    avg_f1 = avg_f1 / splits
-    avg_mse = avg_mse / splits
-    avg_std_dev = avg_std_dev / splits
-    avg_std_error = avg_std_error / splits
-    mse_array.append(avg_mse)
-    std_dev_array.append(avg_std_dev)
-    std_error_array.append(avg_std_error)
-    print('----------------------')
-    print('Average Precision:', avg_precision)
-    print('Average Recall:', avg_recall)
-    print('Average F1', avg_f1)
-    print('Average Mean Squared Error', avg_mse)
-    print('Average Standard Deviation', avg_std_dev)
-    print('Average Standard Error', avg_std_error)
-    print('Runtime:', end - start)
-    return avg_f1
+        for slice in slices:
+            for row in itertools.islice(yelp, slice):
+                clean_row = row[1].strip().replace('"', '').replace(';', '')
+                target_class = row[0]
+                corpus.append(clean_row)
+                y.append(int(target_class))
+            tfidf_vectorizer = TfidfVectorizer(norm='l2')
+            matrix = tfidf_vectorizer.fit_transform(corpus)
+            # sparse = tfidf_vectorizer.fit_transform(corpus).A
+            # print(pd.DataFrame(matrix.todense(), columns=tfidf_vectorizer.get_feature_names()))
+            print(slice)
+            #Cross validation
+            print("CROSS VALIDATION:")
+            splits = 10
+            kf_total = StratifiedKFold(n_splits=splits, shuffle=True)
+            kf = KFold(n_splits=10)
+            y1 = np.array(y)
+            avg_f1 = 0
+            avg_precision = 0
+            avg_recall = 0
+            avg_mse = 0
+            avg_std_dev = 0
+            avg_std_error = 0
+            start = time.time()
+            # for train_index, test_index in kf_total.split(sparse, y1):
+            for train_index, test_index in kf_total.split(matrix, y1):
+                # X_train, X_test = sparse[train_index], sparse[test_index]
+                X_train, X_test = matrix[train_index], matrix[test_index]
+                y_train, y_test = y1[train_index], y1[test_index]
+                classifier.fit(X_train, y_train)
+                y_pred = classifier.predict(X_test)
+                f1 = f1_score(y_test, y_pred, average='macro')  # y_test = true y, y_pred = predicted y with the classifier
+                mse = mean_squared_error(y_test, y_pred);
+                std_dev = np.std([y_test, y_pred])
+                std_error = std_dev/math.sqrt(slice)
+                precision = precision_score(y_test, y_pred, average='macro')
+                recall = recall_score(y_test, y_pred, average='macro')
+                print('F1 score: ', f1)
+                print('Precision score: ', precision)
+                print('Recall score: ', recall)
+                print('Mean squared Error: ', mse)
+                print('Standard Deviation: ', std_dev)
+                print('Standard Error: ', std_error)
+                avg_f1 += f1
+                avg_precision += precision
+                avg_recall += recall
+                avg_mse += mse
+                avg_std_dev += std_dev
+                avg_std_error += std_error
+            end = time.time()
+            avg_recall = avg_recall / splits
+            avg_precision = avg_precision / splits
+            avg_f1 = avg_f1 / splits
+            avg_mse = avg_mse / splits
+            avg_std_dev = avg_std_dev / splits
+            avg_std_error = avg_std_error / splits
+            mse_array.append(avg_mse)
+            std_dev_array.append(avg_std_dev)
+            std_error_array.append(avg_std_error)
+            print('----------------------')
+            print('Average Precision:', avg_precision)
+            print('Average Recall:', avg_recall)
+            print('Average F1', avg_f1)
+            print('Average Mean Squared Error', avg_mse)
+            print('Average Standard Deviation', avg_std_dev)
+            print('Average Standard Error', avg_std_error)
+            print('Runtime:', end - start)
+            # return avg_f1
+            plot_f1.append(avg_f1)
+    return plot_f1
 
 
-for n in N:
-    plot_f1.append(calculate_avg_f1_per_dataset(n))
+# for n in N:
+#     plot_f1.append(calculate_avg_f1_per_dataset(n))
+
+plot_f1 = calculate_f1_array_per_dataset(N)
 
 
+# print('F1 values', plot_f1)
 print('F1 values', plot_f1)
 print('MSE values', mse_array)
 print('Standatd Deviation values', std_dev_array)
@@ -116,8 +127,8 @@ d ={'F1': plot_f1, 'Standard Error': std_error_array}
 df = pd.DataFrame(data=d)
 print(df)
 # plt.ylim(0.80, 0.88)
-plt.show()
+# plt.show()
 
-df.to_csv('results.csv', index = False, header = True)
+# df.to_csv('results.csv', index = False, header = True)
 
 # plt.savefig('polarity_10k_c100.png')
