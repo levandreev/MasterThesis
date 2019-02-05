ec372926b84803eaac9fdd4e01f5c70b8b7ca436
D072828 <duffyduck451@gmail.com>
2019-02-05 14:49:40 +0100

    preprocessing changed, only nouns, not oov, spacy model changed

--- venv/preprocessing.py
+++ venv/preprocessing.py
@@ -3,57 +3,41 @@ import spacy
 import csv
 import itertools
 import time
+import re
 
 corpus = []
 y = []
 N = 1000
 
-nlp = spacy.load('en_core_web_sm') # Language Model
-#
-# df = pd.read_csv('C:/Users/D072828/PycharmProjects/Thesis/venv/train_full.csv', header=None, nrows=100000)
-# ds = df.sample(frac=1)
-# print(ds)
-# ds.to_csv('C:/Users/D072828/PycharmProjects/Thesis/venv/to_be_preprocessed_100_k.csv', index = False, header = False)
+# nlp = spacy.load('en_core_web_sm') # Language Model small
+nlp = spacy.load('en_core_web_md') # Language Model medium
 start = time.time()
 with open('shuffled_yahoo_1k_test.csv', newline='', encoding="utf8") as csvfile:
     yelp = csv.reader(csvfile, delimiter=',')
     for row in itertools.islice(yelp, N):
-         # clean_row = row[1].strip().replace('"','').replace(';','')
          rows = row[1:]
          text_in_doc = ''
+         #Matches any alphanumeric character; this is equivalent to the \w
+         regex = re.compile("[^a-zA-Z0-9_']")
          for e in rows:
              text_in_doc+= e + ' '
-         clean_row = text_in_doc.strip().replace('"','').replace(';','')
-         # clean_row = row[0].strip().replace('"','').replace(';','')
+         clean_row_with_duplicate_ws = regex.sub(' ', text_in_doc).strip()
+         clean_row = re.sub(' +', ' ', clean_row_with_duplicate_ws)
          target_class = row[0]
-         # target_class = clean_row[0]
          corpus.append(clean_row)
-         # corpus.append(clean_row[2:])
          y.append(int(target_class))
-
-# print(y)
-# print(corpus[:2])
-# print(len(corpus)==len(y))
 
 preprocessed_corpus = []
 for row in corpus:
     doc = nlp(row)
     preprocessed_row = ""
     for token in doc:
-        if not token.is_punct and token.is_alpha and (token.tag_ == 'NN' or token.tag_ == 'NNS' or token.tag_ == 'NNP' or token.tag_ == 'NNPS'):
-            # preprocessed_row.append(token.lemma_)
-            preprocessed_row+= (token.lemma_ + ' ')
-            # preprocessed_row+= (token.text + ' ')
-            # print(preprocessed_row)
+        # token.is_oov = token out of vocabulary
+        if not token.is_punct and token.is_alpha and (token.tag_ == 'NN' or token.tag_ == 'NNS' or token.tag_ == 'NNP' or token.tag_ == 'NNPS') and not token.is_oov:
+            preprocessed_row += (token.lemma_ + ' ')
     preprocessed_corpus.append(preprocessed_row)
-    # print(preprocessed_corpus)
-    # print(doc)
-    # preprocessed_corpus.append(doc)
 
-
-# print(len(preprocessed_corpus) == len(y))
-
-d ={'sentiment': y, 'document': preprocessed_corpus}
+d = {'sentiment': y, 'document': preprocessed_corpus}
 df = pd.DataFrame(data=d)
 df.to_csv('preprocessed_yahoo_1k_test.csv', index = False, header = False)
 print(df)
