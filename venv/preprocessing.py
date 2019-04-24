ed4042725ccfc958c5fb68c3b741250731e03494
levandreev <duffyduck451@gmail.com>
2019-04-24 16:09:52 +0200

    Update preprocessing.py

--- venv/preprocessing.py
+++ venv/preprocessing.py
@@ -41,7 +41,7 @@ for row in corpus:
 
 d = {'sentiment': y, 'document': preprocessed_corpus}
 df = pd.DataFrame(data=d)
-df.to_csv('yelp_pol_nouns.csv', index = False, header = False)
+df.to_csv('yelp_pol_all_pos_tags.csv', index = False, header = False)
 print(df)
 end = time.time()
 print('Runtime:', end - start)
