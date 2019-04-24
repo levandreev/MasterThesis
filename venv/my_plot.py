f5218d87ef8fa22b6974373d5a6dcad1f3545309
levandreev <duffyduck451@gmail.com>
2019-04-24 14:54:16 +0200

    Update my_plot.py

--- venv/my_plot.py
+++ venv/my_plot.py
@@ -25,7 +25,7 @@ import math
 # Batches used for SVM
 # N = [10000,20000,30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
 # Batches used for for Naive Bayes
-N = [1000, 5000, 10000, 15000, 20000]
+N = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 plot_f1 = []
 mse_array = []
 std_dev_array = []
@@ -138,11 +138,11 @@ print('Standatd Error values', std_error_array)
 
 d ={'F1': plot_f1, 'Standard Error': std_error_array, 'Accuracy': plot_accuracy}
 df = pd.DataFrame(data=d)
-df.to_csv('results_dpbedia_allpostags_nb_stop_unigram.csv', index = False, header = True)
+df.to_csv('results_dbpedia_allpostags_nb_stop_unigram.csv', index = False, header = True)
 
 plt.errorbar(N, plot_f1, std_error_array, linestyle='None', marker='.')
-plt.savefig('dpbedia_allpostags_nb_stop_unigram_f1.png')
+plt.savefig('dbpedia_allpostags_nb_stop_unigram_f1.png')
 plt.clf()
 plt.errorbar(N, plot_accuracy, std_error_array, linestyle='None', marker='.')
-plt.savefig('dpbedia_allpostags_nb_stop_unigram_acc.png')
+plt.savefig('dbpedia_allpostags_nb_stop_unigram_acc.png')
 
