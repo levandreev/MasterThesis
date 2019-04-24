d9fafb76c6378498fcef9eb699a48531aaf8a795
levandreev <duffyduck451@gmail.com>
2019-04-24 13:40:42 +0200

    Update my_plot.py

--- venv/my_plot.py
+++ venv/my_plot.py
@@ -38,7 +38,7 @@ def calculate_f1_array_per_dataset(slices):
     corpus = []
     y = []
     plot_f1 = []
-    with open('preprocessed_ag_100k_noun.csv', newline='') as csvfile:
+    with open('preprocessed_dbpedia_100k_all_pos_tags.csv', newline='') as csvfile:
         yelp = csv.reader(csvfile, delimiter=',')
         for slice in slices:
             for row in itertools.islice(yelp, slice):
@@ -46,7 +46,7 @@ def calculate_f1_array_per_dataset(slices):
                 target_class = row[0]
                 corpus.append(clean_row)
                 y.append(int(target_class))
-            tfidf_vectorizer = TfidfVectorizer(norm='l2', ngram_range=(1, 1))
+            tfidf_vectorizer = TfidfVectorizer(norm='l2', stop_words='english', ngram_range=(1, 1))
             matrix = tfidf_vectorizer.fit_transform(corpus)
             # sparse = tfidf_vectorizer.fit_transform(corpus).A
             # print(pd.DataFrame(matrix.todense(), columns=tfidf_vectorizer.get_feature_names()))
@@ -138,8 +138,8 @@ df = pd.DataFrame(data=d)
 # df.to_csv('results_polarity_100k.csv', index = False, header = True)
 
 plt.errorbar(N, plot_f1, std_error_array, linestyle='None', marker='.')
-plt.savefig('ag_100k_noun_f1.png')
+plt.savefig('dpbedia_allpostags_svm1_stop_f1.png')
 plt.clf()
 plt.errorbar(N, plot_accuracy, std_error_array, linestyle='None', marker='.')
-plt.savefig('ag_100k_noun_acc.png')
+plt.savefig('dpbedia_allpostags_svm1_stop_acc.png')
 
